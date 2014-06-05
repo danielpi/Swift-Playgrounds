98d6c56b077ceef488a818b313b2fe0926db2a53
Daniel Pink <dr.pink@gmail.com>
2014-06-05 15:22:56 +1000

    Continuing to work through the Swift Tour. Up to the functions section.

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Some experiments with Playgrounds in XCode 6 using the Swift programming language.
 
-- **ASwiftTour.playground** contains the code from the "Swift Tour" chapter of "The Swift Programming Language" book.
+- **ASwiftTour.playground** contains the code from the "Swift Tour" chapter of the book Apple Inc. “The Swift Programming Language.” iBooks. https://itunes.apple.com/au/book/swift-programming-language/id881256329?mt=11
--- Swift-Playgrounds/ASwiftTour.playground/section-1.swift
+++ Swift-Playgrounds/ASwiftTour.playground/section-1.swift
@@ -95,6 +95,98 @@ default:
 
 
 // for-in loop (Also tuples)
+let interestingNumbers = [
+    "Prime": [2,3,5,7,11,13],
+"Fibonacci": [1,1,2,3,5,8],
+   "Square": [1,4,9,16,25],
+]
+var largest = 0
+var largestKind = ""
+for (kind, numbers) in interestingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
+            largestKind = kind
+        }
+    }
+}
+largest
+largestKind
+
+
+// while and do-while loops
+var n = 2
+while n < 100 {
+    n = n * 2
+}
+n
+
+var m = 2
+do {
+    m = m * 2
+} while m < 100
+m
+
+// Indexes in loops and the Range operators
+var firstForLoop = 0
+for i in 0..3 {
+    firstForLoop += i
+}
+firstForLoop
+
+var secondForLoop = 0
+for var i = 0; i < 3; ++i {
+    secondForLoop += 1
+}
+secondForLoop
+
+
+// Functions and Closures
+func greet(name: String, day: String) -> String {
+    return "Hello \(name), today is \(day)."
+}
+greet("Bob", "Tuesday")
+
+func getGasPrices() -> (Double, Double, Double) {
+    return (3.59, 3.69, 3.79)
+}
+getGasPrices()
+
+// A variable number of arguments can be accepted by a function. They are collected into an array for use within the function.
+func sumOf(numbers: Int...) -> Int {
+    var sum = 0
+    for number in numbers {
+        sum += number
+    }
+    return sum
+}
+sumOf()
+sumOf(42, 597, 12)
+
+func meanOf(numbers: Int...) -> Double {
+    var sum = 0
+    for number in numbers {
+        sum += number
+    }
+    let mean = Double(sum) / Double(numbers.count)
+    return mean
+}
+meanOf(42, 597, 12)
+// This is a bit poor. The variable number of arguments being converted into an array means that you can't pass them onto another function. In meanOf above I was unable to reuse sumOf because numbers was an array and sumOf can't accept an array input
+
+// Nested functions
+func returnFifteen() -> Int {
+    var y = 10
+    func add() {
+        y += 5
+    }
+    add()
+    return y
+}
+returnFifteen()
+
+//  Functions are a first-class type. This means that a function can return another function as its value.
+
 
 
 
