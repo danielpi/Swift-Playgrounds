bc64cd607e8f7dd54c39fa4bbbf8f7ecce8f0b4e
Daniel Pink <dr.pink@gmail.com>
2014-06-06 21:40:40 +1000

    Starting on the Basics chapter.

---
+++ Swift-Playgrounds/TheBasics.playground/section-1.swift
@@ -0,0 +1,23 @@
+// The Basics Chapter of “The Swift Programming Language.” iBooks. https://itun.es/au/jEUH0.l
+
+let maximumNumberOfLoginAttempts = 10
+var currentLoginAttempt = 0
+
+var x = 0.0, y = 0.0, z = 0.0
+
+var welcomeMessage: String
+welcomeMessage = "Hello"
+
+let π = 3.14159
+let 你好 = "你好世界"
+let 🐶🐮 = "dogcow"
+
+var friendlyWelcome = "Hello!"
+friendlyWelcome = "Bonjour!"
+
+let languageName = "Swift"
+//languageName = "Swift++" // Compile time error
+
+println(friendlyWelcome)
+
+
