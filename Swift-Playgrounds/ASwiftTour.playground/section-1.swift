016e246e48541ef7e2a1144daa62efda08a60076
Daniel Pink <dr.pink@gmail.com>
2014-06-06 18:56:28 +1000

    Completed the Swift tour code from the start of the Swift book.

--- Swift-Playgrounds/ASwiftTour.playground/section-1.swift
+++ Swift-Playgrounds/ASwiftTour.playground/section-1.swift
@@ -186,7 +186,437 @@ func returnFifteen() -> Int {
 returnFifteen()
 
 //  Functions are a first-class type. This means that a function can return another function as its value.
+func makeIncrementer() -> (Int -> Int) {
+    func addOne(number: Int) -> Int {
+        return 1 + number
+    }
+    return addOne
+}
+var increment = makeIncrementer()
+increment(7)
+
+func hasAnyMatches(list: Int[], condition: Int -> Bool) -> Bool {
+    for item in list {
+        if condition(item) {
+            return true
+        }
+    }
+    return false
+}
+func lessThanTen(number: Int) -> Bool {
+    return number < 10
+}
+var numbers = [20,19,7,12]
+hasAnyMatches(numbers, lessThanTen)
+
+// Closures
+// Use the keyword "in" to seperate the parameters from the body
+numbers.map({
+    (number: Int) -> Int in
+    let result = 3 * number
+    return result
+})
+
+numbers.map({
+    (number:Int) -> Int in
+    if number % 2 == 1 {
+        return 0
+    }
+    return number
+})
+
+// Single statement closures implicitly return the value of their only statement
+numbers.map({ number in 3 * number})
+
+// A closure passed as the last argument to a function can appear immediately after the parentheses
+sort([1,5,3,12,2]){ $0 > $1 }
 
 
+// Objects and Classes
+class Shape {
+    var numberOfSides = 0
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
+
+var shape = Shape()
+shape.numberOfSides = 7
+var shapeDescription = shape.simpleDescription()
+
+class NamedShape {
+    var numberOfSides: Int = 0
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
+// Every property needs a value assigned - either in its declaration (as with numberOfSides) or in the initialiser (as with name). Note the use of self to differentiate between the name property and the name argument.
+
+class Square: NamedShape {
+    var sideLength: Double
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 4
+    }
+    
+    func area() -> Double {
+        return sideLength * sideLength
+    }
+    
+    override func simpleDescription() -> String {
+        return "A square with sides of length \(sideLength)."
+    }
+}
+let test = Square(sideLength:5.2, name:"My test square")
+test.area()
+test.simpleDescription()
+
+class Circle: NamedShape {
+    var radius: Double
+    
+    init(radius: Double, name: String) {
+        self.radius = radius
+        super.init(name: name)
+        numberOfSides = 1
+    }
+    
+    func area() -> Double {
+        return 3.14 * radius * radius
+    }
+    
+    override func simpleDescription() -> String {
+        return "A circle with radius \(radius)"
+    }
+}
+let testCircle = Circle(radius:2.4, name:"my test circle")
+testCircle.area()
+testCircle.simpleDescription()
+
+class EquilateralTriangle: NamedShape {
+    var sideLength: Double = 0.0
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name:name)
+        numberOfSides = 3
+    }
+    
+    var perimeter: Double {
+    get {
+        return 3.0 * sideLength
+    }
+    set (newPerimeter){
+        sideLength = newPerimeter / 3.0
+    }
+    }
+    
+    override func simpleDescription() -> String {
+        return "An equilateral tringle with sides of length \(sideLength)."
+    }
+}
+var triangle = EquilateralTriangle(sideLength:3.3, name:"my test triangle")
+triangle.perimeter
+triangle.perimeter = 1.0
+triangle.sideLength
 
 
+class TriangleAndSquare {
+    var triangle:EquilateralTriangle {
+    willSet {
+        square.sideLength = newValue.sideLength
+    }
+    }
+    
+    var square: Square {
+    willSet {
+        triangle.sideLength = newValue.sideLength
+    }
+    }
+    
+    init(size: Double, name: String) {
+        square = Square(sideLength: size, name: name)
+        triangle = EquilateralTriangle(sideLength: size, name:name)
+    }
+}
+var triangleAndSquare = TriangleAndSquare(size:10, name:"another test shape")
+triangleAndSquare.square.sideLength
+triangleAndSquare.triangle.sideLength
+triangleAndSquare.square = Square(sideLength:50, name:"larger square")
+triangleAndSquare.triangle.sideLength
+
+// You can specify a second parameter name within a method on a class so that the parameter can be refered to by different names when used within the method or outside the method
+class Counter {
+    var count: Int = 0
+    func incrementBy(amount:Int, numberOfTimes times:Int) {
+        count += amount * times
+    }
+}
+var counter = Counter()
+counter.incrementBy(2, numberOfTimes:7)
+
+// Optionals
+// For methods, properties and subscripts you can write a ? afterwards and everything after the question mark will be ignored if the object is nil.
+var optionalSquare: Square? = Square(sideLength: 2.5, name:"optional square")
+var sideLength = optionalSquare?.sideLength
+// let sideLength2 = optionalSquare.sideLength Doesn't work because optionalSquare has a type of Square? not Square. This is what the unwrapping is refering to.
+optionalSquare = nil
+sideLength = optionalSquare?.sideLength
+
+
+// Enumerations and Structures
+enum Rank: Int {
+    case Ace = 1
+    case Two, Three, Four, Fice, Six, Seven, Eight, Nine, Ten
+    case Jack, Queen, King
+    func simpleDescription() -> String {
+        switch self {
+        case .Ace:
+            return "ace"
+        case .Jack:
+            return "jack"
+        case .Queen:
+            return "queen"
+        case .King:
+            return "king"
+        default:
+            return String(self.toRaw())
+        }
+    }
+}
+let ace = Rank.Ace
+let aceRawValue = ace.toRaw()
+ace.simpleDescription()
+let two = Rank.Two
+two.simpleDescription()
+two.toRaw()
+
+func compare(rank: Rank, toRank: Rank) -> Bool {
+    return rank.toRaw() == toRank.toRaw()
+}
+compare(ace, two)
+compare(ace, ace)
+
+if let convertedRank = Rank.fromRaw(3) {
+    let threeDescription = convertedRank.simpleDescription()
+}
+
+// enums don't need to be backed by a raw value if they don't make sense. You can also use Int, Double or String as the raw backing type
+enum Suit {
+    case Spades, Hearts, Diamonds, Clubs
+    
+    func color() -> String {
+        switch self {
+        case .Spades, .Clubs:
+            return "black"
+        case .Hearts, .Diamonds:
+            return "red"
+        }
+    }
+    
+    func simpleDescription() -> String {
+        switch self {
+        case .Spades:
+            return "spades"
+        case .Hearts:
+            return "hearts"
+        case .Diamonds:
+            return "diamonds"
+        case .Clubs:
+            return "clubs"
+        }
+    }
+}
+let hearts = Suit.Hearts
+let heartsDescription = hearts.simpleDescription()
+// hearts.toRaw() // This is an error because Suit doesnt have a raw backing type.
+let heartsColor = hearts.color()
+
+
+// Structures
+// Structures are very similar to classes. They can both have methods and initialisers. One key difference is that structures are always copied when they are passed around in code. Classes are passed by reference.
+struct Card {
+    var rank: Rank
+    var suit: Suit
+    
+    func createFullDeck() -> Card[] {
+        var deck: Card[] = []
+        
+        func appendFullRank(suit: Suit) {
+            for rawRank in 1...13 {
+                let card = Card(rank:Rank.fromRaw(rawRank)!, suit:suit)
+                deck.append(card)
+            }
+        }
+        
+        for suit in [Suit.Hearts, Suit.Diamonds, Suit.Spades, Suit.Clubs] {
+            appendFullRank(suit)
+        }
+        
+        return deck
+    }
+    
+    func simpleDescription() -> String {
+        return "The \(rank.simpleDescription()) of \(suit.simpleDescription())"
+    }
+}
+let threeOfSpades = Card(rank: .Three, suit:.Spades)
+let threeOfSpadesDescription = threeOfSpades.simpleDescription()
+
+let aFullDeck = threeOfSpades.createFullDeck()
+aFullDeck.map({ card in card.simpleDescription() })
+
+// Enumerations
+enum ServerResponse {
+    case Result(String, String)
+    case Error(String)
+}
+
+let success = ServerResponse.Result("6:00am", "8.09pm")
+let failure = ServerResponse.Error("Out of cheese")
+
+switch failure {
+case let .Result(sunrise, sunset):
+    let serverResponse = "Sunrise is at \(sunrise) and sunset is at \(sunset)"
+case let .Error(error):
+    let serverResponse = "Failure... \(error)"
+}
+
+
+// Protocols and Extensions
+protocol ExampleProtocol {
+    var simpleDescription: String { get }
+    mutating func adjust()
+}
+
+class SimpleClass: ExampleProtocol {
+    var simpleDescription: String = "A very simple class."
+    var anotherProperty: Int = 69105
+    func adjust() {
+        simpleDescription += " Now 100% adjusted."
+    }
+}
+var a = SimpleClass()
+a.adjust()
+let aDescription = a.simpleDescription
+
+struct SimpleStructure: ExampleProtocol {
+    var simpleDescription: String = "A simple structure"
+    mutating func adjust() {
+        simpleDescription += " (adjusted)"
+    }
+}
+var b = SimpleStructure()
+b.adjust()
+let bDescription = b.simpleDescription
+
+enum SimpleEnum: Int, ExampleProtocol {
+    case A = 1, B, C, D, E
+    
+    var simpleDescription: String {
+    get {
+        return "A simple enum \(self.toRaw())"
+    }
+    }
+    mutating func adjust() {
+        self = SimpleEnum.fromRaw(self.toRaw() + 1)!
+    }
+}
+var c = SimpleEnum.A
+c.adjust()
+let cDescription = c.simpleDescription
+
+
+// Extensions
+extension Int: ExampleProtocol {
+    var simpleDescription: String {
+        return "The number \(self)"
+    }
+    
+    mutating func adjust() {
+        self += 42
+    }
+}
+7.simpleDescription
+// The 7 above is a literal constant not a variable. Therefore you can't mutate it with adjust.
+
+var someInt = 4
+someInt.adjust()
+someInt
+
+extension Double {
+    var absoluteValue: Double {
+        return copysign(self, 1.0)
+    }
+    
+    var absoluteValue2: Double {
+        return self > 0 ? self : -self
+    }
+}
+
+var aDouble = -7.0
+aDouble.absoluteValue
+
+4.absoluteValue
+-6.0.absoluteValue // Not sure why this doesn't work
+
+let protocolValue: ExampleProtocol = a
+protocolValue.simpleDescription
+// protocolValue.anotherProperty // Uncomment to see the error
+
+
+// Generics
+func repeat<ItemType>(item: ItemType, times: Int) -> ItemType[] {
+    var result = ItemType[]()
+    for i in 0..times {
+        result += item
+    }
+    return result
+}
+repeat("knock", 4)
+
+// Reimplement the Swift standard library's optional type
+enum OptionalValue<T> {
+    case None
+    case Some(T)
+}
+
+var possibleInteger: OptionalValue<Int> = .None
+possibleInteger = .Some(100)
+
+func anyCommonElements <T, U where T: Sequence, U: Sequence, T.GeneratorType.Element: Equatable, T.GeneratorType.Element == U.GeneratorType.Element> (lhs: T, rhs: U) -> Bool {
+    for lhsItem in lhs {
+        for rhsItem in rhs {
+            if lhsItem == rhsItem {
+                return true
+            }
+        }
+    }
+    return false
+}
+anyCommonElements([1,2,3], [3])
+anyCommonElements([1,2,3], [6])
+anyCommonElements([1,2,3], [3.0])
+
+
+func returnAnyCommonElements <T, U where T: Sequence, U: Sequence, T.GeneratorType.Element: Equatable, T.GeneratorType.Element == U.GeneratorType.Element> (lhs: T, rhs: U) -> T.GeneratorType.Element[] {
+    var commonElements: T.GeneratorType.Element[] = []
+    for lhsItem in lhs {
+        for rhsItem in rhs {
+            if lhsItem == rhsItem {
+                commonElements.append(lhsItem)
+            }
+        }
+    }
+    return commonElements
+}
+returnAnyCommonElements([1,2,3], [3])
+returnAnyCommonElements([1,2,3], [6])
+returnAnyCommonElements([1,2,3], [3.0])
