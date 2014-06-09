8920d0f79a7c44c9faec266fb3aecb62221c7cde
Daniel Pink <dr.pink@gmail.com>
2014-06-10 08:49:20 +1000

    Continuing on with the Strings and Characters chapter.

--- Swift-Playgrounds/StringsAndCharacters.playground/section-1.swift
+++ Swift-Playgrounds/StringsAndCharacters.playground/section-1.swift
@@ -5,4 +5,22 @@ let someString = "Some tring literal value"
 let wiseWords = "\"Imagination is more important than knowledge\" - Einstein"
 let dollarSign = "\x24"
 let blackHeart = "\u2665"
+let sparklingHeart = "\U0001F496"
+
+// Empty Strings
+var emptyString = ""
+var anotherEmpyString = String()
+if emptyString.isEmpty {
+    println("Nothing to see here")
+}
+
+// String mutability
+var variableString = "Horse"
+variableString += " and carriage"
+
+let constantString = "Highlander"
+// constantString += " and another Highlander" // There can be only one
+
+
+
 
