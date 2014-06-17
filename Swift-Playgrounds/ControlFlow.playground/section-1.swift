b98ea7ab16bed0e9705ff54c40999a55d2d50ae9
Daniel Pink <dr.pink@gmail.com>
2014-06-17 20:55:25 +1000

    Finished the Control Flow Chapter.

--- Swift-Playgrounds/ControlFlow.playground/section-1.swift
+++ Swift-Playgrounds/ControlFlow.playground/section-1.swift
@@ -230,6 +230,26 @@ println(description)
 
 
 // Labelled Statements
+square = 0
+diceRoll = 0
+board = Int[](count: finalSquare + 1, repeatedValue: 0)
+board[03] = +08; board[06] = +11; board[09] = +09; board[10] = +02
+board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
+
+gameLoop: while square != finalSquare {
+    if ++diceRoll == 7 { diceRoll = 1}
+    switch square + diceRoll {
+    case finalSquare:
+        break gameLoop
+    case let newSquare where newSquare > finalSquare:
+        continue gameLoop
+    default:
+        square += diceRoll
+        square += board[square]
+    }
+}
+println("Game over!")
+
 
 
 
