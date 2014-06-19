137e8402e8d70177bb268e284812c09be0c22b98
Daniel Pink <dr.pink@gmail.com>
2014-06-19 23:25:08 +1000

    Started on the properties chapter

--- Swift-Playgrounds/Properties.playground/section-1.swift
+++ Swift-Playgrounds/Properties.playground/section-1.swift
@@ -1,1 +1,9 @@
 // Properties
+
+// Stored Properties
+struct FixedLengthRange {
+    var firstValue: Int
+    let length: Int
+}
+var rangeOfThreeItems = FixedLengthRange(firstValue: 0, length: 3)
+rangeOfThreeItems.firstValue = 6
