420eab1fad75ec1b303a85c0c224e1e160f2f628
Daniel Pink <dr.pink@gmail.com>
2014-06-21 13:25:33 +1000

    Started on the Initialization Chapter

---
+++ Swift-Playgrounds/Initialization.playground/section-1.swift
@@ -0,0 +1,60 @@
+// Initialization
+
+struct Fahrenheit {
+    var temperature: Double
+    init() {
+        temperature = 32.0
+    }
+}
+var f = Fahrenheit()
+println("The default temperature is \(f.temperature)° Fahrenheit")
+
+
+// Customizing Initialization
+// Initialization Parameters
+struct Celsius {
+    var temperatureInCelsius: Double = 0.0
+    init(fromFahrenheit fahrenheit: Double) {
+        temperatureInCelsius = (fahrenheit - 32) / 1.8
+    }
+    init(fromKelvin kelvin: Double) {
+        temperatureInCelsius = kelvin - 273.15
+    }
+}
+let boilingPointOfWater = Celsius(fromFahrenheit: 212.0)
+let freezingPointOfWater = Celsius(fromKelvin: 273.15)
+
+struct Color {
+    let red = 0.0, green = 0.0, blue = 0.0
+    init(red: Double, green: Double, blue: Double) {
+        self.red   = red
+        self.green = green
+        self.blue  = blue
+    }
+}
+let magenta = Color(red: 1.0, green: 0.0, blue: 1.0)
+//let verGreen = Color(0.0, 1.0, 0.0)
+// Compile time error because external names for parameters were omitted
+
+
+// Optinoal Property Types
+class SurveyQuestion {
+    var text: String
+    var response: String?
+    init(text: String) {
+        self.text = text
+    }
+    func ask() {
+        println(text)
+    }
+}
+let cheeseQuestion = SurveyQuestion(text: "Do you like cheese?")
+cheeseQuestion.ask()
+cheeseQuestion.response = "Yes, I do like cheese."
+
+
+// Modifying Constant Properties during Initialization
+
+
+
+
