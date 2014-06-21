3bcb52cc56d8d7133754ad6b5eef228211cff214
Daniel Pink <dr.pink@gmail.com>
2014-06-21 22:44:44 +1000

    Working through the Initialization chapter

--- Swift-Playgrounds/Initialization.playground/section-1.swift
+++ Swift-Playgrounds/Initialization.playground/section-1.swift
@@ -54,6 +54,70 @@ cheeseQuestion.response = "Yes, I do like cheese."
 
 
 // Modifying Constant Properties during Initialization
+class SurveyQuestion2 {
+    let text: String
+    var response: String?
+    init(text: String) {
+        self.text = text
+    }
+    func ask() {
+        println(text)
+    }
+}
+let beetsQuestion = SurveyQuestion2(text: "How about beets?")
+beetsQuestion.ask()
+beetsQuestion.response = "I also like beets. (But not with cheese.)"
+
+
+// Default Initializer
+class ShoppingListItem {
+    var name: String?
+    var quantity = 1
+    var purchased = false
+}
+var item = ShoppingListItem()
+
+
+// Memberwise Initializers for Structure Types
+// Because both stored properties have a default value, the Size structure automatically receives an init(width:height:) memberwise initializer, which you can use to initialize a new Size instance:
+struct Size {
+    var width = 0.0, height = 0.0
+}
+let twoByTwo = Size(width: 2.0, height: 2.0)
+
+
+// Initializer Delegation for Value Types
+struct Size2 {
+    var width = 0.0, height = 0.0
+}
+struct Point {
+    var x = 0.0, y = 0.0
+}
+struct Rect {
+    var origin = Point()
+    var size = Size2()
+    init() {}
+    init(origin: Point, size: Size2) {
+        self.origin = origin
+        self.size = size
+    }
+    init(center: Point, size: Size2) {
+        let originX = center.x - (size.width / 2)
+        let originY = center.y - (size.height / 2)
+        self.init(origin: Point(x: originX, y: originY), size: size)
+    }
+}
+let basicRect = Rect()
+let originRect = Rect(origin: Point(x: 2.0, y: 2.0), size: Size2(width: 5.0, height: 5.0))
+let centerRect = Rect(center: Point(x: 4.0, y: 4.0), size: Size2(width: 3.0, height: 3.0))
+
+
+// Class Inheritance and Initialization
+
+
+
+
+
 
 
 
