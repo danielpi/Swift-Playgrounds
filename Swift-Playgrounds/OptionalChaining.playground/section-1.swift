45e283c7dde10644f45de3aa74848cc2d69d5034
Daniel Pink <dr.pink@gmail.com>
2014-06-28 22:57:16 +1000

    Working throught the chapter on Optional Chaining

--- Swift-Playgrounds/OptionalChaining.playground/section-1.swift
+++ Swift-Playgrounds/OptionalChaining.playground/section-1.swift
@@ -18,3 +18,91 @@ if let roomCount = john.residence?.numberOfRooms {
     println("Unable to retreive the number of rooms.")
 }
 
+// Defining Model Classes for Optional Chaining
+class Person2 {
+    var residence: Residence2?
+}
+
+class Residence2 {
+    var rooms = Room[]()
+    var numberOfRooms: Int {
+    return rooms.count
+    }
+    subscript(i: Int) -> Room {
+        return rooms[i]
+    }
+    func printNumberOfRooms() {
+        println("The number of rooms is \(numberOfRooms)")
+    }
+    var address: Address?
+}
+
+class Room {
+    let name: String
+    init(name: String) { self.name = name }
+}
+
+class Address {
+    var buildingName: String?
+    var buildingNumber: String?
+    var street: String?
+    func buildingIdentifier() -> String? {
+        if buildingName {
+            return buildingName
+        } else if buildingNumber {
+            return buildingNumber
+        } else {
+            return nil
+        }
+    }
+}
+
+
+// Calling Properties Through Optional Chaining
+let jack = Person2()
+if let roomCount = john.residence?.numberOfRooms {
+    println("John's residence has \(roomCount) room(s).")
+} else {
+    println("Unable to retreive the number of rooms.")
+}
+
+
+// Calling Methods through Optional Chaining
+if jack.residence?.printNumberOfRooms() {
+    println("It was possible to print the number of rooms.")
+} else {
+    println("It was not possible to print the number of rooms.")
+}
+// printNumberOfRooms returns Void? if called on an optional parent value
+
+
+// Calling Subscripts Through Optional Chaining
+if let firstRoomName = jack.residence?[0].name {
+    println("The first room name is \(firstRoomName).")
+} else {
+    println("Unable to retrieve the first room name.")
+}
+
+let jacksHouse = Residence2()
+jacksHouse.rooms += Room(name: "Living Room")
+jacksHouse.rooms += Room(name: "Kitchen")
+jack.residence = jacksHouse
+
+if let firstRoomName = jack.residence?[0].name {
+    println("The first room name is \(firstRoomName).")
+} else {
+    println("Unable to retrieve the first room name.")
+}
+
+
+// Linking Multiple levels of Chaining
+
+
+
+
+
+
+
+
+
+
