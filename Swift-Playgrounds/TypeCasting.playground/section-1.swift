c90116720b8835557d5ccc5534f5a41ba298515a
Daniel Pink <dr.pink@gmail.com>
2014-07-01 11:11:49 +1000

    Finished the Type Casting chapter

--- Swift-Playgrounds/TypeCasting.playground/section-1.swift
+++ Swift-Playgrounds/TypeCasting.playground/section-1.swift
@@ -57,11 +57,54 @@ for item in library {
 
 
 // Type Casting for Any and AnyObject
+let someObjects: AnyObject[] = [
+    Movie(name: "2001: A Space Odyssey", director: "Stanley Kubrick"),
+    Movie(name: "Moon", director:"Duncan Jones"),
+    Movie(name: "Alien", director: "Ridley Scott")
+]
+//  Because this array is known to contain only Movie instances, you can downcast and unwrap directly to a non-optional Movie with the forced version of the type cast operator (as):
+for object in someObjects {
+    let movie = object as Movie
+    println("Movie: '\(movie.name)', dir.\(movie.director)")
+}
+for movie in someObjects as Movie[] {
+    println("Movie: '\(movie.name)', dir. \(movie.director)")
+}
 
 
+// Any
+var things = Any[]()
+things.append(0)
+things.append(0.0)
+things.append(42)
+things.append(3.1459)
+things.append("hello")
+things.append((3.0, 5.0))
+things.append(Movie(name: "Ghostbusters", director: "Ivan Reitman"))
 
-
-
+for thing in things {
+    switch thing {
+    case 0 as Int:
+        println("zero as an Int")
+    case 0 as Double:
+        println("zero as a Double")
+    case let someInt as Int:
+        println("an integer value of \(someInt)")
+    case let someDouble as Double where someDouble > 0:
+        println("a positive double value of \(someDouble)")
+    case is Double:
+        println("some other double value that I don't want to print")
+    case let someString as String:
+        println("a string value of \"\(someString)\"")
+    case let (x, y) as (Double, Double):
+        println("an (x, y) point at \(x), \(y)")
+    case let movie as Movie:
+        println("a movie called '\(movie.name)', dir. \(movie.director)")
+    default:
+        println("something else")
+    }
+}
+//  The cases of a switch statement use the forced version of the type cast operator (as, not as?) to check and cast to a specific type. This check is always safe within the context of a switch case statement.
 
 
 
