41d4e239a9ee2e8c9329d6c83357a7bab352de9f
Daniel Pink <dr.pink@gmail.com>
2014-07-09 22:35:20 +1000

    Continuing to work through the Cocoa in Swift book

--- Swift-Playgrounds/WritingSwiftClassesWithObjectiveCBehaviour.playground/section-1.swift
+++ Swift-Playgrounds/WritingSwiftClassesWithObjectiveCBehaviour.playground/section-1.swift
@@ -16,3 +16,23 @@ class MySwiftViewController: UIViewController {
 
 
 // Adopting Protocols
+/*
+class AnotherSwiftViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+}
+*/
+
+
+// Interacting with Interface Builder
+// Working with Oulets and Actions
+//
+//  When you declare an outlet in Swift, 
+//  - the compiler automatically converts the type to a weak implicitly unwrapped optional
+//  - assigns it an initial value of nil
+
+//  The compiler replaces @IBOutlet var name: Type with @IBOutlet weak var name: Type! = nil. 
+//  The compiler converts the type to an implicitly unwrapped optional so that you aren’t required to assign a value in an initializer. It is implicitly unwrapped because after your class is initialized from a storyboard or xib file, you can assume that the outlet has been connected. Outlets are weak by default because the outlets you create usually have weak relationships.
+class YetAnotherSwiftViewController: UIViewController {
+    @IBO
+}
+
