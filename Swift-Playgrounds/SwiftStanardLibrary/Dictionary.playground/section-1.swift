8a2a825b0b13880c8c047342ccdf64910b752510
Daniel Pink <dr.pink@gmail.com>
2014-07-14 22:55:17 +1000

    Working through the section on Dictionaries

---
+++ Swift-Playgrounds/SwiftStanardLibrary/Dictionary.playground/section-1.swift
@@ -0,0 +1,52 @@
+// Swift Standard Library - Types - Dictionary
+
+var emptyDictionary = Dictionary<String, Int>()
+var equivilentEmptyDictionary = [String: Int]()
+var anotherEmptyDictionary = Dictionary<String, Int>(minimumCapacity: 10)
+var literalDictionary = ["a":1]
+
+
+// Accessing and Changing Dictionary Elements
+var dictionary = ["one": 1, "two": 2, "three": 3]
+let value = dictionary["two"]
+if let unwrappedValue = dictionary["three"] {
+    println("The integer value for \"three\" was: \(unwrappedValue)")
+    unwrappedValue
+}
+
+dictionary["three"] = 33
+dictionary
+// You can add to a dictionary using subscripting
+dictionary["four"] = 4
+dictionary
+// you can remove a value for a key by setting it to nil
+dictionary["three"] = nil
+dictionary
+// You can't change, add or remove elements to a constant dictionary
+
+dictionary = ["one": 1, "two": 2, "three": 3]
+let previousValue = dictionary.updateValue(22, forKey: "two")
+dictionary
+if let unwrappedPreviousValue = dictionary.updateValue(33, forKey: "three") {
+    println("Replaced the previous value: \(unwrappedPreviousValue)")
+} else {
+    println("Added a new value")
+}
+
+dictionary = ["one": 1, "two": 2, "three": 3]
+let prevValue = dictionary.removeValueForKey("two")
+if let unwrappedPreviousValue = dictionary.removeValueForKey("three") {
+    println("Removed the old value: \(unwrappedPreviousValue)")
+} else {
+    println("Didn't find a value for the given key to delete")
+}
+
+dictionary.removeAll()
+dictionary
+
+
+// Querying a Dictionary
+
+
+
+
