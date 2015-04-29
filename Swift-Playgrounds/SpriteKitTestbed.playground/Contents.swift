f4fdfc81e493202890f1e267385f79b81bbcf1e5
Daniel Pink <dr.pink@gmail.com>
2015-04-29 12:13:28 +1000

    Added some motion to the SpriteKit experimentation

--- Swift-Playgrounds/SpriteKitTestbed.playground/Contents.swift
+++ Swift-Playgrounds/SpriteKitTestbed.playground/Contents.swift
@@ -2,6 +2,10 @@ import Cocoa
 import SpriteKit
 import XCPlayground
 
+
+// Credit to http://ericasadun.com/2015/04/22/swift-simple-spritekit-experimentation/
+
+
 // Build scene and view
 let sceneSize = CGSizeMake(400.0, 300.0)
 let view = SKView(frame: CGRect(origin: CGPointZero, size: sceneSize))
@@ -15,3 +19,5 @@ shape1.fillColor = SKColor.redColor()
 scene.addChild(shape1)
 shape1.position = CGPointMake(200.0, 150.0)
 
+var action = SKAction.repeatActionForever(SKAction.moveBy(CGVectorMake(0.02, 0.1), duration: 0.01))
+shape1.runAction(action)
