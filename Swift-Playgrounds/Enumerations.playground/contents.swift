b8b3925d6af06e885315f504779e0e50f1f8334e
Daniel Pink <dr.pink@gmail.com>
2017-01-25 21:42:49 +1000

    Updated the Enumerations chapter

--- Swift-Playgrounds/Enumerations.playground/contents.swift
+++ Swift-Playgrounds/Enumerations.playground/contents.swift
@@ -3,37 +3,37 @@
 //  An enumeration defines a common type for a group of related values and enables you to work with those values in a type-safe way within your code.
 
 enum CompassPoint {
-    case North
-    case South
-    case East
-    case West
+    case north
+    case south
+    case east
+    case west
 }
 
 enum Planet {
-    case Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune
+    case mercury, venus, earth, mars, jupiter, saturn, uranus, neptune
 }
 
-var directionToHead = CompassPoint.West
+var directionToHead = CompassPoint.west
 
-directionToHead = .East
+directionToHead = .east
 
 
 // Matching Enumeration Values with a Switch Statement
-directionToHead = .South
+directionToHead = .south
 switch directionToHead {
-case .North:
+case .north:
     print("Lots of planets have a north")
-case .South:
+case .south:
     print("Watch out for penguins")
-case .East:
+case .east:
     print("Where the sun rises")
-case .West:
+case .west:
     print("Where the skies are blue")
 }
 
-let somePlanet = Planet.Earth
+let somePlanet = Planet.earth
 switch somePlanet {
-case .Earth:
+case .earth:
     print("Mostly Harmless")
 default:
     print("Not a safe place for humans")
@@ -41,57 +41,57 @@ default:
 
 // Associated Values
 enum Barcode {
-    case UPCA(Int, Int, Int)
-    case QRCode(String)
+    case upc(Int, Int, Int, Int)
+    case qrCode(String)
 }
-var productBarcode = Barcode.UPCA(8, 85909_51226, 3)
-productBarcode = .QRCode("ABCDEFGHIJKLMNOP")
+var productBarcode = Barcode.upc(8, 85909, 51226, 3)
+productBarcode = .qrCode("ABCDEFGHIJKLMNOP")
 
 switch productBarcode {
-case .UPCA(let numberSystem, let identifier, let check):
-    print("UPC-A with value of \(numberSystem), \(identifier), \(check)")
-case .QRCode(let productCode):
-    print("QR code with value of \(productCode).")
+case .upc(let numberSystem, let manufacturer, let product, let check):
+    print("UPC: \(numberSystem), \(manufacturer), \(product), \(check)")
+case .qrCode(let productCode):
+    print("QR code: \(productCode).")
 }
 
-productBarcode = Barcode.UPCA(8, 85909_51226, 3)
+productBarcode = Barcode.upc(8, 85909, 51226, 3)
 switch productBarcode {
-case let .UPCA(numberSystem, identifier, check):
-    print("UPC-A with value of \(numberSystem), \(identifier), \(check)")
-case let .QRCode(productCode):
-    print("QR code with value of \(productCode).")
+case let .upc(numberSystem, manufacturer, product, check):
+    print("UPC : \(numberSystem), \(manufacturer), \(product), \(check).")
+case let .qrCode(productCode):
+    print("QR code: \(productCode).")
 }
 
 
 // Raw Values
 enum ASCIIControlCharacter: Character {
-    case Tab = "\t"
-    case LineFeed = "\n"
-    case CarriageReturn = "\r"
+    case tab = "\t"
+    case lineFeed = "\n"
+    case carriageReturn = "\r"
 }
 
 // Implicitly Assigned Raw Values
 enum PlanetRaw: Int {
-    case Mercury = 1, Venuc, Earth, Mars, Jupiter, Saturn, Uranus, Neptune
+    case mercury = 1, venus, earth, mars, jupiter, saturn, uranus, neptune
 }
 
-let earthsOrder = PlanetRaw.Earth.rawValue
+let earthsOrder = PlanetRaw.earth.rawValue
 // earthsOrder is 3
 
 enum CompassPointRaw: String {
-    case North, South, East, West
+    case north, south, east, west
 }
-let sunsetDirection = CompassPointRaw.West.rawValue
+let sunsetDirection = CompassPointRaw.west.rawValue
 // sunsetDirection is "West"
 
 
 // Initializing from a Raw Value
 let possiblePlanet = PlanetRaw(rawValue: 7)
 
-let positionToFind = 9
+let positionToFind = 11
 if let somePlanet = PlanetRaw(rawValue: positionToFind) {
     switch somePlanet {
-    case .Earth:
+    case .earth:
         print("Mostly harmless")
     default:
         print("Not a safe place for humans")
@@ -103,32 +103,34 @@ if let somePlanet = PlanetRaw(rawValue: positionToFind) {
 
 // Recursive Enumerations
 enum ArithmeticExpression {
-    case Number(Int)
-    indirect case Addition(ArithmeticExpression, ArithmeticExpression)
-    indirect case Multiplication(ArithmeticExpression, ArithmeticExpression)
+    case number(Int)
+    indirect case addition(ArithmeticExpression, ArithmeticExpression)
+    indirect case multiplication(ArithmeticExpression, ArithmeticExpression)
 }
 
 indirect enum ArithmeticExpression2 {
-    case Number(Int)
-    case Addition(ArithmeticExpression2, ArithmeticExpression2)
-    case Multiplication(ArithmeticExpression2, ArithmeticExpression2)
+    case number(Int)
+    case addition(ArithmeticExpression2, ArithmeticExpression2)
+    case multiplication(ArithmeticExpression2, ArithmeticExpression2)
 }
 
-func evaluate(expression: ArithmeticExpression) -> Int {
+let five = ArithmeticExpression.number(5)
+let four = ArithmeticExpression.number(4)
+let sum = ArithmeticExpression.addition(five, four)
+let product = ArithmeticExpression.multiplication(sum, ArithmeticExpression.number(2))
+
+func evaluate(_ expression: ArithmeticExpression) -> Int {
     switch expression {
-    case .Number(let value):
+    case .number(let value):
         return value
-    case let .Addition(lhs, rhs):
+    case let .addition(lhs, rhs):
         return evaluate(lhs) + evaluate(rhs)
-    case let .Multiplication(lhs, rhs):
+    case let .multiplication(lhs, rhs):
         return evaluate(lhs) * evaluate(rhs)
     }
 }
 
-let five = ArithmeticExpression.Number(5)
-let four = ArithmeticExpression.Number(4)
-let sum = ArithmeticExpression.Addition(five, four)
-let product = ArithmeticExpression.Multiplication(sum, ArithmeticExpression.Number(2))
 print(evaluate(product))
+
 
 
