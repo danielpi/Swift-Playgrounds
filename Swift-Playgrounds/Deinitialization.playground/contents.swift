743d282c09271604343f2e36427970b987be407d
Daniel Pink <dr.pink@gmail.com>
2017-02-02 17:14:55 +1000

    Updating the deinit section for Swift 3

--- Swift-Playgrounds/Deinitialization.playground/contents.swift
+++ Swift-Playgrounds/Deinitialization.playground/contents.swift
@@ -4,36 +4,33 @@
 
 struct Bank {
     static var coinsInBank = 10_000
-    static func vendCoins(var numberOfCoinsToVend: Int) -> Int {
-        numberOfCoinsToVend = min(numberOfCoinsToVend, coinsInBank)
+    static func distribute(coins numberOfCoinsRequested: Int) -> Int {
+        let numberOfCoinsToVend = min(numberOfCoinsRequested, coinsInBank)
         coinsInBank -= numberOfCoinsToVend
         return numberOfCoinsToVend
     }
-    static func receiveCoins(coins: Int) {
+    static func receive(coins: Int) {
         coinsInBank += coins
     }
 }
 
 class Player {
     var coinsInPurse: Int
-    
     init(coins: Int) {
-        coinsInPurse = Bank.vendCoins(coins)
+        coinsInPurse = Bank.distribute(coins: coins)
     }
-    
-    func winCoins(coins: Int) {
-        coinsInPurse += Bank.vendCoins(coins)
+    func win(coins: Int) {
+        coinsInPurse += Bank.distribute(coins: coins)
     }
-    
     deinit {
-        Bank.receiveCoins(coinsInPurse)
+        Bank.receive(coins: coinsInPurse)
     }
 }
 var playerOne: Player? = Player(coins: 100)
 print("A new player has joined the game with \(playerOne!.coinsInPurse) coins")
 print("There are now \(Bank.coinsInBank) coins left in the bank")
 
-playerOne!.winCoins(2_000)
+playerOne!.win(coins: 2_000)
 print("PlayerOne won 2000 coins & now has \(playerOne!.coinsInPurse) coins")
 print("The bank now only has \(Bank.coinsInBank) coins left")
 
