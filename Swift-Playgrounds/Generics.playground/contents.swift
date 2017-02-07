94d03a7dbfa47da717b95fc9d2c568ff39b542a9
Daniel Pink <dr.pink@gmail.com>
2017-02-07 10:38:49 +1000

    Generics section updated to Swift 3

--- Swift-Playgrounds/Generics.playground/contents.swift
+++ Swift-Playgrounds/Generics.playground/contents.swift
@@ -2,7 +2,7 @@
 
 // The Problem That Generics Solve
 //  standard, non-generic function
-func swapTwoInts(inout a: Int, inout b: Int) {
+func swapTwoInts(_ a: inout Int, _ b: inout Int) {
     let temporaryA = a
     a = b
     b = temporaryA
@@ -10,18 +10,18 @@ func swapTwoInts(inout a: Int, inout b: Int) {
 
 var someInt = 3
 var anotherInt = 107
-swapTwoInts(&someInt, b: &anotherInt)
+swapTwoInts(&someInt, &anotherInt)
 print("someInt is now \(someInt), and anotherInt is now \(anotherInt)")
 
 //  The swapTwoInts function is useful, but it can only be used with Int values. If you want to swap two String values, or two Double values, you have to write more functions, such as the swapTwoStrings and swapTwoDoubles functions shown below
 
-func swapTwoStrings(inout a: String, inout b: String) {
+func swapTwoStrings(_ a: inout String, _ b: inout String) {
     let temporaryA = a
     a = b
     b = temporaryA
 }
 
-func swapTwoDoubles(inout a: Double, inout b: Double) {
+func swapTwoDoubles(_ a: inout Double, _ b: inout Double) {
     let temporaryA = a
     a = b
     b = temporaryA
@@ -29,18 +29,18 @@ func swapTwoDoubles(inout a: Double, inout b: Double) {
 
 
 // Generic Functions
-func swapTwoValues<T>(inout a: T, inout b: T) {
+func swapTwoValues<T>(_ a: inout T, _ b: inout T) {
     let temporaryA = a
     a = b
     b = temporaryA
 }
 
-swapTwoValues(&someInt, b: &anotherInt)
+swapTwoValues(&someInt, &anotherInt)
 print("someInt is now \(someInt), and anotherInt is now \(anotherInt)")
 
 var someString = "hello"
 var anotherString = "world"
-swapTwoValues(&someString, b: &anotherString)
+swapTwoValues(&someString, &anotherString)
 print("someString is now \(someString), and anotherString is now \(anotherString)")
 
 
@@ -48,7 +48,7 @@ print("someString is now \(someString), and anotherString is now \(anotherString
 // Non-generic version of a Stack first
 struct IntStack {
     var items = [Int]()
-    mutating func push(item: Int) {
+    mutating func push(_ item: Int) {
         items.append(item)
     }
     mutating func pop() -> Int {
@@ -57,12 +57,12 @@ struct IntStack {
 }
 
 // Now a Generic version
-struct Stack<T> {
-    var items = [T]()
-    mutating func push(item: T) {
+struct Stack<Element> {
+    var items = [Element]()
+    mutating func push(_ item: Element) {
         items.append(item)
     }
-    mutating func pop() -> T {
+    mutating func pop() -> Element {
         return items.removeLast()
     }
 }
@@ -75,6 +75,17 @@ stackOfStrings
 
 let fromTheTop = stackOfStrings.pop()
 
+// Extending a Generic Type
+
+extension Stack {
+    var topItem: Element? {
+        return items.isEmpty ? nil : items[items.count - 1]
+    }
+}
+
+if let topItem = stackOfStrings.topItem {
+    print("The top item on the stack is \(topItem).")
+}
 
 // Type Constraints
 /*
@@ -82,8 +93,8 @@ func someFunction<T: SomeClass, U: SomeProtocol>(someT: T, someU: U) {
     // function body goes here
 }
 */
-func findStringIndex(array: [String], valueToFind: String) -> Int? {
-    for (index, value) in array.enumerate() {
+func findIndex(ofString valueToFind: String, in array: [String]) -> Int? {
+    for (index, value) in array.enumerated() {
         if value == valueToFind {
             return index
         }
@@ -91,42 +102,42 @@ func findStringIndex(array: [String], valueToFind: String) -> Int? {
     return nil
 }
 let strings = ["cat", "dog", "llama", "parakeet", "terrapin"]
-if let foundIndex = findStringIndex(strings, valueToFind: "llama") {
+if let foundIndex = findIndex(ofString: "llama", in: strings) {
     print("The index of llama is \(foundIndex)")
 }
 
-func findIndex<T:Equatable>(array: [T], valueToFind: T) -> Int? {
-    for (index, value) in array.enumerate() {
+func findIndex<T:Equatable>(of valueToFind: T, in array: [T]) -> Int? {
+    for (index, value) in array.enumerated() {
         if value == valueToFind {
             return index
         }
     }
     return nil
 }
-let doubleIndex = findIndex([3.14159, 0.1, 0.25], valueToFind: 9.3)
-let stringIndex = findIndex(["Mike", "Malcolm", "Andrea"], valueToFind: "Andrea")
+let doubleIndex = findIndex(of: 9.3, in: [3.14159, 0.1, 0.25])
+let stringIndex = findIndex(of: "Andrea", in: ["Mike", "Malcolm", "Andrea"])
 
 
 // Associated Types
 protocol Container {
-    associatedtype ItemType
-    mutating func append(item: ItemType)
+    associatedtype Item
+    mutating func append(_ item: Item)
     var count: Int { get }
-    subscript(i: Int) -> ItemType {get }
+    subscript(i: Int) -> Item { get }
 }
 
 struct IntStack2: Container {
     // original IntStack implementation
     var items = [Int]()
-    mutating func push(item: Int) {
+    mutating func push(_ item: Int) {
         items.append(item)
     }
     mutating func pop() -> Int {
         return items.removeLast()
     }
     // conformance to the Container protocol
-    typealias ItemType = Int
-    mutating func append(item: Int) {
+    typealias Item = Int
+    mutating func append(_ item: Int) {
         self.push(item)
     }
     var count: Int {
@@ -137,23 +148,23 @@ struct IntStack2: Container {
     }
 }
 
-struct Stack2<T>: Container {
+struct Stack2<Element>: Container {
     // original Stack<T> implementation
-    var items = [T]()
-    mutating func push(item: T) {
+    var items = [Element]()
+    mutating func push(_ item: Element) {
         items.append(item)
     }
-    mutating func pop() -> T {
+    mutating func pop() -> Element {
         return items.removeLast()
     }
     // conformance to the Container protocol
-    mutating func append(item: T) {
+    mutating func append(_ item: Element) {
         self.push(item)
     }
     var count: Int {
         return items.count
     }
-    subscript(i: Int) -> T {
+    subscript(i: Int) -> Element {
         return items[i]
     }
 }
@@ -163,8 +174,10 @@ struct Stack2<T>: Container {
 extension Array: Container {}
 
 
-// Where Clauses
-func allItemsMatch<C1: Container, C2: Container where C1.ItemType == C2.ItemType, C1.ItemType: Equatable>(someContainer: C1, anotherContainer: C2) -> Bool {
+// Generic Where Clauses
+func allItemsMatch<C1: Container, C2: Container>
+    (_ someContainer: C1, _ anotherContainer: C2) -> Bool
+    where C1.Item == C2.Item, C1.Item: Equatable {
     
     // check that both containers contain the same number of items
     if someContainer.count != anotherContainer.count {
@@ -188,8 +201,33 @@ stackOfStrings2.push("tres")
 
 var arrayOfStrings = ["uno", "dos", "tres"]
 
-if allItemsMatch(stackOfStrings2, anotherContainer: arrayOfStrings) {
+if allItemsMatch(stackOfStrings2, arrayOfStrings) {
     print("All items match.")
 } else {
     print("Not all items match.")
 }
+
+
+// Extensions with a Generic Where Clause
+extension Stack2 where Element: Equatable {
+    func isTop(_ item: Element) -> Bool {
+        guard let topItem = items.last else {
+            return false
+        }
+        return topItem == item
+    }
+}
+
+if stackOfStrings2.isTop("tres") {
+    print("Top element is tres.")
+} else {
+    print("Top element is something else.")
+}
+
+// If you try to call the isTop(_:) method on a stack whose elements aren't equatable, you'll get a compile-time error.
+struct NotEquatable { }
+var notEquatableStack = Stack2<NotEquatable>()
+let notEquatableValue = NotEquatable()
+notEquatableStack.push(notEquatableValue)
+// notEquatableStack.isTop(notEquatableValue) // Error
+
